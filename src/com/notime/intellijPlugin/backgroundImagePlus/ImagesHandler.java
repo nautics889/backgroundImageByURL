3a5c09b9cc77bd4e347402fc6c8284d864be6ecf
nautics889 <cyberukr@gmail.com>
2020-03-25 23:20:09 +0200

    updated ``ImagesHandlerSingleton``: read text-file line by line and push to image list

--- src/com/notime/intellijPlugin/backgroundImagePlus/ImagesHandler.java
+++ src/com/notime/intellijPlugin/backgroundImagePlus/ImagesHandler.java
@@ -1,7 +1,10 @@
 package com.notime.intellijPlugin.backgroundImagePlus;
 
 import javax.activation.MimetypesFileTypeMap;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -44,24 +47,14 @@ class ImagesHandler {
     }
     
     private void collectImages(List<String> images, String folder) {
-        File root = new File(folder);
-        if (!root.exists()) {
-            return;
-        }
-        File[] list = root.listFiles();
-        if (list == null) {
-            return;
-        }
-        
-        for (File f : list) {
-            if (f.isDirectory()) {
-                collectImages(images, f.getAbsolutePath());
-            } else {
-                if (!isImage(f)) {
-                    continue;
-                }
-                images.add(f.getAbsolutePath());
+        File file = new File(folder);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                images.add(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
--- src/com/notime/intellijPlugin/backgroundImagePlus/ImagesHandlerSingleton.java
+++ src/com/notime/intellijPlugin/backgroundImagePlus/ImagesHandlerSingleton.java
@@ -1,7 +1,12 @@
 package com.notime.intellijPlugin.backgroundImagePlus;
 
+import org.apache.commons.collections.CollectionUtils;
+
 import javax.activation.MimetypesFileTypeMap;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,53 +28,48 @@ public enum ImagesHandlerSingleton {
     
     private String lastFolder = null;
     
-    public String getRandomImage(String folder) {
+    public String getRandomImage(String filePath) {
         // 文件夹改动或者完成了一次循环
-        if (!folder.equals(lastFolder) || randomImageList == null || randomImageList.size() == 0) {
-            randomImageList = this.getRandomImageList(folder);
+        if (!filePath.equals(lastFolder) || CollectionUtils.isEmpty(randomImageList)) {
+            randomImageList = this.getRandomImageList(filePath);
         }
-        lastFolder = folder;
-        while (randomImageList != null && randomImageList.size() > 0 && !isImage(new File(randomImageList.get(0)))) {
-            randomImageList.remove(0);
-        }
-        return randomImageList == null || randomImageList.size() == 0 ? null : randomImageList.remove(0);
+        lastFolder = filePath;
+//        while (CollectionUtils.isNotEmpty(randomImageList) && !isImage(new File(randomImageList.get(0)))) {
+//            randomImageList.remove(0);
+//        }
+        return CollectionUtils.isEmpty(randomImageList) ? null : randomImageList.remove(0);
     }
     
     public void resetRandomImageList() {
         randomImageList = lastFolder == null ? null : this.getRandomImageList(lastFolder);
     }
     
-    private List<String> getRandomImageList(String folder) {
-        if (folder.isEmpty()) {
+    private List<String> getRandomImageList(String filePath) {
+        if (filePath.isEmpty()) {
             return null;
         }
         List<String> images = new ArrayList<>();
-        collectImages(images, folder);
+        collectImages(images, filePath);
         Collections.shuffle(images);
         return images;
     }
     
-    private void collectImages(List<String> images, String folder) {
-        File root = new File(folder);
-        if (root.exists()) {
-            File[] list = root.listFiles();
-            if (list != null) {
-                for (File f : list) {
-                    if (f.isDirectory()) {
-                        collectImages(images, f.getAbsolutePath());
-                    } else {
-                        if (isImage(f)) {
-                            images.add(f.getAbsolutePath());
-                        }
-                    }
-                }
+    private void collectImages(List<String> images, String filePath) {
+        File file = new File(filePath);
+        NotificationCenter.notice("Everything is OK! " + file.toPath());
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                images.add(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
-    private boolean isImage(File file) {
-        String[] parts = typeMap.getContentType(file).split("/");
-        return parts.length != 0 && "image".equals(parts[0]);
-    }
+//    private boolean isImage(File file) {
+//        String[] parts = typeMap.getContentType(file).split("/");
+//        return parts.length != 0 && "image".equals(parts[0]);
+//    }
     
 }
